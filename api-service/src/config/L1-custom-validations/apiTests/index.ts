fe873591bd4d05e8dcd299f3f19d173685ee0989
vaibhavwits <vaibhav.sharma2@thewitslab.com>
2026-03-03 11:36:46 +0530

    fix

--- api-service/src/config/L1-custom-validations/apiTests/index.ts
+++ api-service/src/config/L1-custom-validations/apiTests/index.ts
@@ -1,34 +1,34 @@
-import { cancel } from "./cancel";
-import { confirm } from "./confirm";
-import { init } from "./init";
-import { onCancelRouter } from "./on_cancel";
-import { on_confirm } from "./on_confirm";
-import { onInit } from "./on_init";
-import { onSearch } from "./on_search";
-import { onSelect } from "./on_select";
-import { onStatusRouter } from "./on_status";
-import { search } from "./search";
-import { select } from "./select";
-import { checkStatus } from "./status";
-import { track } from "./track";
-import { on_track } from "./on_track";
-import { onUpdateRouter, updateRouter } from "./update";
+// import { cancel } from "./cancel";
+// import { confirm } from "./confirm";
+// import { init } from "./init";
+// import { onCancelRouter } from "./on_cancel";
+// import { on_confirm } from "./on_confirm";
+// import { onInit } from "./on_init";
+// import { onSearch } from "./on_search";
+// import { onSelect } from "./on_select";
+// import { onStatusRouter } from "./on_status";
+// import { search } from "./search";
+// import { select } from "./select";
+// import { checkStatus } from "./status";
+// import { track } from "./track";
+// import { on_track } from "./on_track";
+// import { onUpdateRouter, updateRouter } from "./update";
 
-export {
-  cancel,
-  confirm,
-  init,
-  onCancelRouter,
-  on_confirm,
-  onInit,
-  onSearch,
-  onSelect,
-  onStatusRouter,
-  search,
-  select,
-  checkStatus,
-  track,
-  on_track,
-  onUpdateRouter,
-  updateRouter,
-};
+// export {
+//   cancel,
+//   confirm,
+//   init,
+//   onCancelRouter,
+//   on_confirm,
+//   onInit,
+//   onSearch,
+//   onSelect,
+//   onStatusRouter,
+//   search,
+//   select,
+//   checkStatus,
+//   track,
+//   on_track,
+//   onUpdateRouter,
+//   updateRouter,
+// };

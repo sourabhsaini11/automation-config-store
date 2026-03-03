432c4db8c19a90bf0d60a831c030551992b93eb2
vaibhavwits <vaibhav.sharma2@thewitslab.com>
2026-03-03 11:59:55 +0530

    fix

--- api-service/src/config/L1-custom-validations/apiTests/on_cancel/index.ts
+++ api-service/src/config/L1-custom-validations/apiTests/on_cancel/index.ts
@@ -3,52 +3,52 @@ import { addActionToRedisSet } from "../../utils/helper";
 import { onCancel } from "./on_cancel";
 
 export const onCancelRouter = async (data: any) => {
-  let result: any = [];
-  let actionCall = `${ApiSequence.ON_CANCEL}`;
-  try {
-    const onStatusPresent = await addActionToRedisSet(
-      data.context.transaction_id,
-      ApiSequence.ON_STATUS_OUT_FOR_DELIVERY,
-      ApiSequence.ON_CANCEL_RTO
-    );
+  // let result: any = [];
+  // let actionCall = `${ApiSequence.ON_CANCEL}`;
+  // try {
+  //   const onStatusPresent = await addActionToRedisSet(
+  //     data.context.transaction_id,
+  //     ApiSequence.ON_STATUS_OUT_FOR_DELIVERY,
+  //     ApiSequence.ON_CANCEL_RTO
+  //   );
     
-    if (onStatusPresent) {
-      actionCall = ApiSequence.ON_CANCEL_RTO;
-    }
+  //   if (onStatusPresent) {
+  //     actionCall = ApiSequence.ON_CANCEL_RTO;
+  //   }
 
-    const cancelPresent = await addActionToRedisSet(
-      data.context.transaction_id,
-      ApiSequence.CANCEL,
-      ApiSequence.ON_CANCEL
-    );
+  //   const cancelPresent = await addActionToRedisSet(
+  //     data.context.transaction_id,
+  //     ApiSequence.CANCEL,
+  //     ApiSequence.ON_CANCEL
+  //   );
 
-    if (cancelPresent) {
-      actionCall = `${ApiSequence.ON_CANCEL}`;
-    }
+  //   if (cancelPresent) {
+  //     actionCall = `${ApiSequence.ON_CANCEL}`;
+  //   }
 
 
-  } catch (error: any) {
-    console.error(
-      `!!Error while previous action call /${constants.ON_CANCEL}, ${error.stack}`
-    );
-  }
+  // } catch (error: any) {
+  //   console.error(
+  //     `!!Error while previous action call /${constants.ON_CANCEL}, ${error.stack}`
+  //   );
+  // }
 
-  switch (actionCall) {
-    case `${ApiSequence.ON_CANCEL}`:
-      result = await onCancel(data, "4");
-      break;
-    case `${ApiSequence.ON_CANCEL_RTO}`:
-      result = await onCancel(data, "5");
-      break;
-    default:
-      result = [
-        {
-          valid: false,
-          code: 400,
-          description: `Invalid on_cancel call`,
-        },
-      ];
-      break;
-  }
-  return result;
+  // switch (actionCall) {
+  //   case `${ApiSequence.ON_CANCEL}`:
+  //     result = await onCancel(data, "4");
+  //     break;
+  //   case `${ApiSequence.ON_CANCEL_RTO}`:
+  //     result = await onCancel(data, "5");
+  //     break;
+  //   default:
+  //     result = [
+  //       {
+  //         valid: false,
+  //         code: 400,
+  //         description: `Invalid on_cancel call`,
+  //       },
+  //     ];
+  //     break;
+  // }
+  // return result;
 };

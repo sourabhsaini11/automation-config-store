078733a70d195f438bac0c1039c3175ac53a74d2
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2026-04-08 16:29:40 +0530

    fix: removing L1 custom validations

--- api-service/src/config/L1-custom-validations/index.ts
+++ api-service/src/config/L1-custom-validations/index.ts
@@ -1,21 +1,21 @@
-import {
-  cancel,
-  confirm,
-  init,
-  onCancelRouter,
-  on_confirm,
-  onInit,
-  onSearch,
-  onSelect,
-  onStatusRouter,
-  search,
-  select,
-  checkStatus,
-  track,
-  on_track,
-  onUpdateRouter,
-  updateRouter,
-} from "./apiTests";
+// import {
+//   cancel,
+//   confirm,
+//   init,
+//   onCancelRouter,
+//   on_confirm,
+//   onInit,
+//   onSearch,
+//   onSelect,
+//   onStatusRouter,
+//   search,
+//   select,
+//   checkStatus,
+//   track,
+//   on_track,
+//   onUpdateRouter,
+//   updateRouter,
+// } from "./apiTests";
 
 import { validationOutput } from "./types";
 
@@ -30,58 +30,58 @@ export async function performL1CustomValidations(
   console.log("Performing custom L1 validations for action: " + action);
   let result: any = [];
   switch (action) {
-    case "search":
-      result = await search(payload);
-      break;
-    case "on_search":
-      result = await onSearch(payload);
-      break;
-    case "select":
-      result = await select(payload);
-      break;
-    case "on_select":
-      result = await onSelect(payload);
-      break;
-    case "init":
-      result = await init(payload);
-      break;
-    case "on_init":
-      result = await onInit(payload);
-      break;
-    case "confirm":
-      result = await confirm(payload);
-      break;
-    case "on_confirm":
-      result = await on_confirm(payload);
-      break;
-    case "status":
-      result = await checkStatus(payload);
-      break;
-    case "on_status":
-      result = await onStatusRouter(payload);
-      break;
-    case "track":
-      result = await track(payload);
-      break;
-    case "on_track":
-      result = await on_track(payload);
-      break;
-    case "cancel":
-      result = await cancel(payload);
-      break;
-    case "on_cancel":
-      result = await onCancelRouter(payload);
-      break;
-    case "update":
-      result = await updateRouter(payload);
-      break;
-    case "on_update":
-      result = await onUpdateRouter(payload);
-      break;
-    case "issue":
-    case "on_issue":
-    case "on_issue_status":  
-      return result;
+    // case "search":
+    //   result = await search(payload);
+    //   break;
+    // case "on_search":
+    //   result = await onSearch(payload);
+    //   break;
+    // case "select":
+    //   result = await select(payload);
+    //   break;
+    // case "on_select":
+    //   result = await onSelect(payload);
+    //   break;
+    // case "init":
+    //   result = await init(payload);
+    //   break;
+    // case "on_init":
+    //   result = await onInit(payload);
+    //   break;
+    // case "confirm":
+    //   result = await confirm(payload);
+    //   break;
+    // case "on_confirm":
+    //   result = await on_confirm(payload);
+    //   break;
+    // case "status":
+    //   result = await checkStatus(payload);
+    //   break;
+    // case "on_status":
+    //   result = await onStatusRouter(payload);
+    //   break;
+    // case "track":
+    //   result = await track(payload);
+    //   break;
+    // case "on_track":
+    //   result = await on_track(payload);
+    //   break;
+    // case "cancel":
+    //   result = await cancel(payload);
+    //   break;
+    // case "on_cancel":
+    //   result = await onCancelRouter(payload);
+    //   break;
+    // case "update":
+    //   result = await updateRouter(payload);
+    //   break;
+    // case "on_update":
+    //   result = await onUpdateRouter(payload);
+    //   break;
+    // case "issue":
+    // case "on_issue":
+    // case "on_issue_status":  
+    //   return result;
     default:
       result = [
         {

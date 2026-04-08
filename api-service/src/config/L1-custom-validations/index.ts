46a5db1bc43224502f3b04f6c8cb9be6b83eba98
Sahil Sharma <sahil-ondc@users.noreply.github.com>
2026-04-08 16:15:51 +0530

    fix: removing L1 custom validations

--- api-service/src/config/L1-custom-validations/index.ts
+++ api-service/src/config/L1-custom-validations/index.ts
@@ -1,10 +1,10 @@
 import { validationOutput } from "./types";
-import search from "./apiTests/search";
-import onSearch from "./apiTests/on_search";
-import select from "./apiTests/select";
-import onSelect from "./apiTests/on_select";
-import init from "./apiTests/init";
-import onInit from "./apiTests/on_init";
+// import search from "./apiTests/search";
+// import onSearch from "./apiTests/on_search";
+// import select from "./apiTests/select";
+// import onSelect from "./apiTests/on_select";
+// import init from "./apiTests/init";
+// import onInit from "./apiTests/on_init";
 // import { confirm } from "./apiTests/confirm";
 // import { cancel } from "./apiTests/cancel";
 
@@ -18,18 +18,18 @@ export function performL1CustomValidations(
   console.log("Performing custom L1 validations for action: " + action);
 
   switch (action) {
-    case "search":
-      return search(payload);
-    case "on_search":
-      return onSearch(payload);
-    case "select":
-      return select(payload);
-    case "on_select":
-      return onSelect(payload);
-    case "init":
-      return init(payload);
-    case "on_init":
-      return onInit(payload);
+    // case "search":
+    //   return search(payload);
+    // case "on_search":
+    //   return onSearch(payload);
+    // case "select":
+    //   return select(payload);
+    // case "on_select":
+    //   return onSelect(payload);
+    // case "init":
+    //   return init(payload);
+    // case "on_init":
+    //   return onInit(payload);
     // case "confirm":
     //   return confirm(payload);
     // case "on_confirm":

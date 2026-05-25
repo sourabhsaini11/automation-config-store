1aee95b81d333d7eb48e63806fcf07f12682fd2d
sourabhsaini11 <ss1841620@gmail.com>
2026-05-25 15:17:20 +0530

    fix/1104

--- mock-service/src/config/mock-config/TRV12/Intercity/2.0.0/on_init/on_init_error_response/generator.ts
+++ mock-service/src/config/mock-config/TRV12/Intercity/2.0.0/on_init/on_init_error_response/generator.ts
@@ -1,7 +1,28 @@
+function parseSecondsOnly(durationStr: string) {
+  const regex = /^PT(\d+)S$/; // Only matches "PT<number>S"
+  const match = durationStr.match(regex);
+
+  if (!match) {
+    throw new Error("Invalid format (only PT#S supported)");
+  }
+
+  const seconds = parseInt(match[1], 10);
+
+  // Optional safety clamp (e.g., max 60s)
+  if (seconds > 60) {
+    throw new Error("TTL too large, max 60s allowed");
+  }
+
+  return seconds * 1000; // milliseconds for setTimeout
+}
+
 export async function initGenerator(existingPayload: any, sessionData: any) {
   if (existingPayload.message) {
     existingPayload.error = existingPayload.message.error;
   }
+  const ttlwait = parseSecondsOnly(existingPayload.context.ttl)
+  await new Promise(r => setTimeout(r, ttlwait));
   const { error, context } = existingPayload;
+
   return { error, context };
 }
